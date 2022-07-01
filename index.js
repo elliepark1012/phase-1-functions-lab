20ef21750456596a0a0c82f7ef62b1091a856708
Ellie <elliepark1012@gmail.com>
2022-07-01 16:30:22 -0400

    commit tree 7f01befef24b20838818f2c244f89f209b50b113

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(location){
+    if (location >= 42){
+    return location-42
+    }
+    else if (location < 42){
+        return 42-location
+    }
+}
+
+function distanceFromHqInFeet(location){
+    return (distanceFromHqInBlocks(location))*264
+}
+
+function distanceTravelledInFeet(start, destination) {
+    if (destination >= start) {
+        return (destination - start)*264  
+    }
+    else if (start > destination) {
+        return (start -destination)*264
+    }
+    } 
+
+function calculatesFarePrice(start,destination) {
+    if (distanceTravelledInFeet(start,destination) <= 400) {
+        return 0
+    } 
+    else if (distanceTravelledInFeet(start,destination) > 400 && distanceTravelledInFeet(start,destination) <= 2000){
+        return  ((distanceTravelledInFeet(start,destination))-400)*0.02
+    }  
+    else if (distanceTravelledInFeet(start,destination) > 2000 && distanceTravelledInFeet(start,destination) <= 2500){
+        return 25
+    } 
+    else if (distanceTravelledInFeet(start,destination) > 2500){
+        return 'cannot travel that far'
+    }
+}
